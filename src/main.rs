253866644c6292f8457f4dfce35c9c25b0e37d3b
Han Hossain <ahanuf12@gmail.com>
2022-01-17 21:33:55 -0800

    simple regex test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use regex::Regex;
+
+fn main() -> anyhow::Result<()> {
+    let re = Regex::new(r"(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})")?;
+    let caps = re.captures("2010-03-14").unwrap();
+    println!("captures: {:?}", caps);
+    println!("year: {}", &caps["year"]);
+    println!("month: {}", &caps["month"]);
+    println!("day: {}", &caps["day"]);
+    Ok(())
 }
