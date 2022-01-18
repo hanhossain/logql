f5759957af73a021338e652cf069e6b30ade4184
Han Hossain <ahanuf12@gmail.com>
2022-01-17 21:59:29 -0800

    simple yaml test

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
+use anyhow::Result;
 use regex::Regex;
+use std::collections::HashMap;
 
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<()> {
+    print_regex()?;
+    print_yaml()?;
+
+    Ok(())
+}
+
+fn print_regex() -> Result<()> {
     let re = Regex::new(r"(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})")?;
     let caps = re.captures("2010-03-14").unwrap();
     println!("captures: {:?}", caps);
     println!("year: {}", &caps["year"]);
     println!("month: {}", &caps["month"]);
     println!("day: {}", &caps["day"]);
+    Ok(())
+}
+
+fn print_yaml() -> Result<()> {
+    let yaml = "x: 1.0\ny: 2.0\n";
+    let deserialized: HashMap<String, f64> = serde_yaml::from_str(yaml)?;
+    println!("{:?}", deserialized);
     Ok(())
 }
