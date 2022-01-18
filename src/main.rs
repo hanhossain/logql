f15bb8dfc0d12773274e42b92e1fec7b17f10e22
Han Hossain <ahanuf12@gmail.com>
2022-01-17 23:45:41 -0800

    remove unnecessary functions

--- src/main.rs
+++ src/main.rs
@@ -48,20 +48,3 @@ struct Schema {
     regex: String,
     columns: Vec<String>,
 }
-
-fn print_regex() -> Result<()> {
-    let re = Regex::new(r"(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})")?;
-    let caps = re.captures("2010-03-14").unwrap();
-    println!("captures: {:?}", caps);
-    println!("year: {}", &caps["year"]);
-    println!("month: {}", &caps["month"]);
-    println!("day: {}", &caps["day"]);
-    Ok(())
-}
-
-fn print_yaml() -> Result<()> {
-    let yaml = "x: 1.0\ny: 2.0\n";
-    let deserialized: HashMap<String, f64> = serde_yaml::from_str(yaml)?;
-    println!("{:?}", deserialized);
-    Ok(())
-}
