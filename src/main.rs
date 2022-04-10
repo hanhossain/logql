7c129c539a4c3a9e9f72e01d740f81be25deecc8
Han Hossain <ahanuf12@gmail.com>
2022-04-10 01:43:17 -0700

    print with table

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use crate::parser::values::Type;
 use crate::parser::Parser;
+use comfy_table::{presets, ContentArrangement, Table};
 
 mod error;
 mod parser;
@@ -9,6 +11,12 @@ fn main() -> anyhow::Result<()> {
 1\tfirst\t42.0
 2\tsecond\t3.14
 this should not match the regex therefore this should be part of the extra text
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. In non fringilla tortor, vitae bibendum \
+nisl. Nullam quis auctor tellus. Cras nisi enim, vehicula semper luctus in, placerat at tellus. \
+Aenean commodo est purus, aliquet fringilla turpis volutpat id. Nam tristique venenatis ex eu \
+lobortis. Curabitur tempus mattis lorem, quis fringilla metus consectetur tristique. Suspendisse \
+vitae euismod justo. Aenean sollicitudin gravida sapien id pharetra. Vivamus nec ex et metus \
+gravida tempus et sit amet purus. Praesent bibendum varius imperdiet.
 3\tthird\t10.1
 4\tfourth\t20.2
 nomatch 20 4
@@ -23,6 +31,7 @@ columns:
       type: i32
     - name: string_value
       type: string
+      multiline: true
     - name: double_value
       type: string
 ";
@@ -30,7 +39,43 @@ columns:
     let parser = Parser::try_from(schema)?;
     let parsed = parser.parse(source.lines());
 
-    println!("{:#?}", parsed);
+    let mut table = Table::new();
+    let mut header: Vec<_> = parser
+        .schema
+        .columns
+        .iter()
+        .map(|c| c.name.to_owned())
+        .collect();
+    header.push(String::from("extraText"));
+    table
+        .load_preset(presets::UTF8_FULL)
+        .set_content_arrangement(ContentArrangement::DynamicFullWidth)
+        .set_header(header);
 
+    for row in parsed {
+        let mut result: Vec<_> = parser
+            .schema
+            .columns
+            .iter()
+            .map(|c| &row.values[&c.name.as_str()])
+            .map(|t| match t {
+                Type::String(x) => x.to_string(),
+                Type::Int32(x) => x.to_string(),
+                Type::Int64(x) => x.to_string(),
+                Type::Bool(x) => x.to_string(),
+            })
+            .collect();
+        if let Some(extra_text) = row.extra_text {
+            let mut combined_text = String::new();
+            for text in extra_text {
+                combined_text.push_str(text);
+                combined_text.push('\n');
+            }
+            result.push(combined_text);
+        }
+        table.add_row(result);
+    }
+
+    println!("{table}");
     Ok(())
 }
